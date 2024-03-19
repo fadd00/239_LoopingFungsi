36e11a6ede692670bf12650f6f55df99a98b5801
fadd00 <andhikahutama9@gmail.com>
2024-03-19 15:24:38 +0700

    membuat input dan mentotalkan barang

--- jordan.cpp
+++ jordan.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 string nama;
 int Nadidas, Npuma, Ncompass, Nnike;
-int Hadidas, Hpuma, Hcompass, Hbike;
+int Hadidas, Hpuma, Hcompass, Hnike;
+
+void input(){
+
+    cout << "masukan jumlah adidas = ";
+    cin >> Nadidas;
+    cout << "masukan jumlah  puma = ";
+    cin >> Npuma;
+    cout << "masukan jumlah compass = ";
+    cin >> Ncompass;
+    cout << "masukan jumlah nike = ";
+    cin >> Nnike;
+
+} int totalHarga(){
+    return (Nadidas * Hadidas) + (Npuma * Hpuma) + (Ncompass * Hcompass) + (Nnike * hnike) 
+}
 
 int main(){
 
